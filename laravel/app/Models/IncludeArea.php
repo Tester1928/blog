c3c8f2683cfc804228f70fe4e76aa3de609365d3
yura <rozzhalovets.yura@mail.ru>
2023-01-03 23:03:25 +0300

    create migrations and Models

---
+++ laravel/app/Models/IncludeArea.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class IncludeArea extends Model
+{
+    use HasFactory;
+}
